1ca086a28b7447bb1318790553597841b6302e9b
Mike Owino <mike.owino40@yahoo.com>
2021-09-21 22:46:41 +0300

    add file

---
+++ src/gallery-default.tsx
@@ -0,0 +1,36 @@
+import PhotoswipeUIDefault from 'photoswipe/dist/photoswipe-ui-default'
+import React, { useRef, FC } from 'react'
+import { CustomGallery, DefaultLayout, layoutPropTypes, LayoutProps } from '.'
+import { baseGalleryPropTypes, BaseGalleryProps } from './gallery-base'
+
+export type GalleryProps = BaseGalleryProps & LayoutProps
+
+/**
+ * Gallery component with default Layout and UI
+ */
+export const Gallery: FC<GalleryProps> = ({
+  children,
+  options,
+  id,
+  onOpen,
+  ...restProps
+}) => {
+  const defaultLayoutRef = useRef<HTMLElement>()
+  return (
+    <CustomGallery
+      layoutRef={defaultLayoutRef}
+      ui={PhotoswipeUIDefault}
+      options={options}
+      id={id}
+      onOpen={onOpen}
+    >
+      {children}
+      <DefaultLayout {...restProps} ref={defaultLayoutRef} />
+    </CustomGallery>
+  )
+}
+
+Gallery.propTypes = {
+  ...baseGalleryPropTypes,
+  ...layoutPropTypes,
+}
